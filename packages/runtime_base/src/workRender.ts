c5b71576641ef2957c3c8c9e17af854767d37903
daihe.dh <daihe.dh@autonavi.com>
2022-07-19 15:12:12 +0800

    feat: 计算无需移动新节点白名单逻辑完善
    描述（需求点 or 原因）：
    修改内容：
    影响范围：
    是否历史问题：
    
    fix #ID

--- packages/runtime_base/src/workRender.ts
+++ packages/runtime_base/src/workRender.ts
@@ -1516,36 +1516,48 @@ export function cacheRenderPayload(
  * @param map 
  */
 export function computeUnmovedWhitelist(map: Record<number, number>): Record<number, true> {
-  const seq: string[] = []
+  const whitelist: Record<number, true> = createEmptyObject()
+  const lis: string[] = []
   // 记录在递增子序列中索引 i 对应的上一元素所对应的索引
   const correctMap: Record<number, string> = createEmptyObject()
   for (const i in map) {
     const value: number = map[i]
-    let lastIndex: string = seq[seq.length - 1]
+    let lastIndex: string = lis[lis.length - 1]
     const lastValue: number = lastIndex ?
       map[lastIndex] :
       -1
     if (value > lastValue) {
       // 当前值大于递增子序列最后一个索引对应的值，满足贪心条件，将该元素
       // 索引记录到递增子序列中，并记录对应的纠正锚点
-      seq.push(i)
-      correctMap[i] = seq[seq.length - 2]
+      lis.push(i)
+      correctMap[i] = lis[lis.length - 2]
     } else {
       // 二分查找递增子序列中小于当前元素且最接近当前元素的索引，保证找到的索引
       // 及之前的索引对应的值能和当前元素组成正确顺序的递增序列
       let start: number = 0
-      let end: number = seq.length - 1
+      let end: number = lis.length - 1
       let middle: number
-      while (start >= end) {
+      while (start + 1 >= end) {
         middle = (start + end) >> 1
-        if (value > map[seq[middle]]) {
+        if (value > map[lis[middle]]) {
           start = middle
         } else {
           end = middle
         }
       }
 
-
+      // 二分查找后 [0,start] 区间内的子序列元素均小于当前元素，可构成正确顺序的
+      // 递增子序列
+      lis[end] = i
+      correctMap[i] = lis[start]
     }
   }
+
+  // 回溯递增子序列，根据先前得到的索引纠正 map，由后向前递推索引值并更新索引值
+  for (let i = lis.length - 2; i >= 0; i--) {
+    lis[i] = correctMap[lis[i + 1]]
+    whitelist[map[lis[i]]] = true
+  }
+
+  return whitelist
 }
